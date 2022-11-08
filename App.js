a8fb3735b4ad2b603ffa3ae52abc546d4a20c505
Gabriel Dias <gabrielmoreiradias270402@gmail.com>
2022-11-08 20:58:57 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {View, Text, Button, TextInput } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import Home from './components/Home'
+
+const Pilha = createNativeStackNavigator();
+
+function telaLogin(props){
+  return(
+    <View>
+      <Text>
+        Tela Login
+      </Text>
+      <TextInput placeholder="E-mail1111" style={{backgroundColor: "gray", borderWidth: 2, margin: 10}}/>
+      <TextInput placeholder="Senha" style={{backgroundColor: "gray", borderWidth: 2, margin: 10}}/>
+      <Button
+      title="Entrar"
+      onPress = {()=>props.navigation.navigate('telaHome')}/>
+      <Button 
+      title="Cadastrar"
+      onPress = {()=>props.navigation.navigate('telaCadastro')}/>
+    </View>
+  )
+}
+
+function telaCadastro(props){
+  return(
+    <View>
+      <Text>
+        Tela Cadastro
+      </Text>
+      <Button 
+      title="Login"
+      onPress = {()=>props.navigation.navigate('telaLogin')}/>
+    </View>
+  )
+}
+
+export default App = () => {
+  return(
+    <NavigationContainer>
+    <Pilha.Navigator>
+      <Pilha.Screen 
+      name="telaLogin" component={telaLogin} options={{title: 'Tela Login'}}/>
+      <Pilha.Screen
+      name="telaCadastro" component={telaCadastro} options={{title: 'Tela Cadastro'}}/>
+      <Pilha.Screen
+      name="telaHome" component={Home} options={{title: 'Tela Home'}}/>
+    </Pilha.Navigator>
+  
+    </NavigationContainer>
+  )
+}
---
+++ components/Home.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Tab = createBottomTabNavigator()
+
+function telaDash(props){
+  return(
+    <View>
+      <Text>Dash</Text>
+    </View>
+  )
+}
+
+function telaMaps(props){
+  return(
+    <View>
+      <Text>Maps</Text>
+    </View>
+  )
+}
+
+function telaConfig(props){
+  return(
+    <View>
+      <Text>Configurações</Text>
+    </View>
+  )
+}
+
+
+
+export default App = ()=>{
+  return(
+    <NavigationContainer independent={true}>
+      <Tab.Navigator>
+        <Tab.Screen
+        name="telaDash"
+        component={telaDash}
+        options={{
+          tabBarLabel: "Dash",
+          tabBarIcon: () => (
+            <Icon name="dashboard" size={30}/>
+          )
+          }}
+          />
+
+        <Tab.Screen
+        name="telaMaps"
+        component={telaMaps}
+        options={{
+          tabBarLabel: "Maps",
+          tabBarIcon: () => (
+            <Icon name="map-marker" size={30}/>
+          )
+          }}/>
+
+        <Tab.Screen
+        name="telaConfig"
+        component={telaConfig}
+        options={{
+          tabBarLabel: "Config",
+          tabBarIcon: ()=>(
+            <Icon name="gear" size={30}/>
+          )
+          }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
